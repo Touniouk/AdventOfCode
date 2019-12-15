78b074dcaa333439f8c72f08f9cb93335b9b0f20
Luc <touniouk@yahoo.com>
2019-12-15 01:52:07 +0100

    finished 10 part 1

--- src/AdventOfCode/Probleme10.java
+++ src/AdventOfCode/Probleme10.java
@@ -19,33 +19,32 @@ public class Probleme10 {
         // Building angle maps
         asteroidList.forEach(a -> a.buildDistanceMap(asteroidList));
         asteroidList.forEach(a -> a.buildAngleMap(asteroidList));
-
-        System.out.println(asteroidList.get(0).distanceMap);
-        System.out.println("Asteroid location: " +  asteroidList.get(0));
-        for (Map.Entry e : asteroidList.get(0).angleMap.entrySet()) {
-            for (Asteroid a : (Set<Asteroid>) e.getValue()) {
-                System.out.println("Asteroid " + a + ", angle " + e.getKey() + ", distance " + asteroidList.get(0).distanceMap.get(a));
-            }
-        }
+        asteroidList.forEach(Asteroid::buildVisibleList);
+        // Get the one with the most other visible asteroids
+//        asteroidList.forEach(a -> System.out.println(a + " -> " + a.getVisibleList()));
+//        System.out.println("==================");
+        Asteroid best = asteroidList.stream().max(Comparator.comparingInt(a -> a.getVisibleList().size())).get();
+        System.out.println("The best positioned asteroid is " + best + " with " + best.getVisibleList().size() + " other asteroids visible");
     }
 }
 
 class Asteroid {
-    Coords coords;
-    Map<Asteroid, Coords> distanceMap;
-    Map<Double, Set<Asteroid>> angleMap;
+    private Coords coords;
+    private Map<Asteroid, Coords> distanceMap;
+    private Map<Double, Set<Asteroid>> angleMap;
+    private List<Asteroid> visibleList;
 
     Asteroid(int x, int y) {
-        angleMap = new HashMap<>();
-        distanceMap = new HashMap<>();
         coords = new Coords(x, y);
     }
 
     void buildDistanceMap(List<Asteroid> asteroids) {
-        asteroids.forEach(a -> distanceMap.put(a, a.coords.minus(this.coords)));
+        distanceMap = new HashMap<>();
+        asteroids.forEach(a -> distanceMap.put(a, Coords.minus(a.coords, this.coords)));
     }
 
     void buildAngleMap(List<Asteroid> asteroids) {
+        angleMap = new HashMap<>();
         for (Asteroid a : asteroids) {
             double angle, x = distanceMap.get(a).x, y = distanceMap.get(a).y;
             if (x == 0 && y == 0) continue;
@@ -53,35 +52,56 @@ class Asteroid {
             else if (x == 0 && y < 0) angle = -90d;
             else if (y == 0 && x < 0) angle = 180;
             else angle = Math.toDegrees(Math.atan((double) distanceMap.get(a).y / (double) distanceMap.get(a).x));
-            if (x > 0 && y < 0) angle += 180;
-            else if (x < 0 && y > 0) angle += 180;
+//            if (x > 0 && y < 0) angle += 180;
+            if (x < 0 && y > 0) angle += 180;
             else if (x < 0 && y < 0) angle -= 180;
             angleMap.computeIfAbsent(angle, k -> new HashSet<>());
             angleMap.get(angle).add(a);
         }
     }
 
+    void buildVisibleList() {
+        visibleList = new ArrayList<>();
+        angleMap.forEach((k,v) -> visibleList.add(v.stream().min((a1, a2) -> Coords.distanceDifference(distanceMap.get(a1), distanceMap.get(a2))).get()));
+    }
+
+    public List<Asteroid> getVisibleList() {
+        return visibleList;
+    }
+
+    public Map<Double, Set<Asteroid>> getAngleMap() {
+        return angleMap;
+    }
+
+    public Map<Asteroid, Coords> getDistanceMap() {
+        return distanceMap;
+    }
+
     @Override
     public String toString() {
         return coords.toString();
     }
+}
 
-    class Coords {
-        int x;
-        int y;
+class Coords {
+    int x;
+    int y;
 
-        Coords(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+    Coords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-        Coords minus(Coords coord) {
-            return new Coords(this.x-coord.x, this.y-coord.y);
-        }
+    static Coords minus(Coords c1, Coords c2) {
+        return new Coords(c1.x-c2.x, c1.y-c2.y);
+    }
 
-        @Override
-        public String toString() {
-            return "[" + x + "," + y + "]";
-        }
+    static int distanceDifference(Coords c1, Coords c2) {
+        return Math.abs(c1.x) + Math.abs(c1.y) - Math.abs(c2.x) - Math.abs(c2.y);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + x + "," + y + "]";
     }
 }
