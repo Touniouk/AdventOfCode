2c7b233b86a5b173935f15179d451845275c74c9
Luc <touniouk@yahoo.com>
2019-12-15 21:24:05 +0100

    finished problem 10

--- src/AdventOfCode/Probleme10.java
+++ src/AdventOfCode/Probleme10.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Probleme10 {
@@ -19,20 +18,18 @@ public class Probleme10 {
         // Building angle maps
         asteroidList.forEach(a -> a.buildDistanceMap(asteroidList));
         asteroidList.forEach(a -> a.buildAngleMap(asteroidList));
-        asteroidList.forEach(Asteroid::buildVisibleList);
         // Get the one with the most other visible asteroids
-//        asteroidList.forEach(a -> System.out.println(a + " -> " + a.getVisibleList()));
-//        System.out.println("==================");
-        Asteroid best = asteroidList.stream().max(Comparator.comparingInt(a -> a.getVisibleList().size())).get();
-        System.out.println("The best positioned asteroid is " + best + " with " + best.getVisibleList().size() + " other asteroids visible");
+        Asteroid best = asteroidList.stream().max(Comparator.comparingInt(a -> a.getAngleMap().size())).get();
+        Asteroid vaporizedAsteroid = best.getVaporizedAsteroid(200, false);
+        System.out.println("The best positioned asteroid is " + best + " with " + best.getAngleMap().size() + " other asteroids visible");
+        System.out.println("The 200th asteroid to be vaporized is " + vaporizedAsteroid + " (" + vaporizedAsteroid.getNumberRepresentation() + ")");
     }
 }
 
 class Asteroid {
     private Coords coords;
     private Map<Asteroid, Coords> distanceMap;
-    private Map<Double, Set<Asteroid>> angleMap;
-    private List<Asteroid> visibleList;
+    private Map<Double, Queue<Asteroid>> angleMap;
 
     Asteroid(int x, int y) {
         coords = new Coords(x, y);
@@ -44,37 +41,48 @@ class Asteroid {
     }
 
     void buildAngleMap(List<Asteroid> asteroids) {
-        angleMap = new HashMap<>();
+        angleMap = new TreeMap<>();
         for (Asteroid a : asteroids) {
             double angle, x = distanceMap.get(a).x, y = distanceMap.get(a).y;
             if (x == 0 && y == 0) continue;
-            else if (x == 0 && y > 0) angle = 90d;
-            else if (x == 0 && y < 0) angle = -90d;
-            else if (y == 0 && x < 0) angle = 180;
-            else angle = Math.toDegrees(Math.atan((double) distanceMap.get(a).y / (double) distanceMap.get(a).x));
-//            if (x > 0 && y < 0) angle += 180;
-            if (x < 0 && y > 0) angle += 180;
-            else if (x < 0 && y < 0) angle -= 180;
-            angleMap.computeIfAbsent(angle, k -> new HashSet<>());
+            else if (x == 0 && y < 0) angle = 0d;
+            else if (x == 0 && y > 0) angle = 180d;
+            else {
+                angle = Math.toDegrees(Math.atan((double) distanceMap.get(a).x / (double) distanceMap.get(a).y));
+                if (y >= 0) angle = 180 - angle;
+                else if (x > 0) angle *= -1;
+                else if (x < 0) angle = 360 - angle;
+            }
+            angleMap.computeIfAbsent(angle, k -> new PriorityQueue<>((a1, a2) -> Coords.distanceDifference(distanceMap.get(a1), distanceMap.get(a2))));
             angleMap.get(angle).add(a);
         }
     }
 
-    void buildVisibleList() {
-        visibleList = new ArrayList<>();
-        angleMap.forEach((k,v) -> visibleList.add(v.stream().min((a1, a2) -> Coords.distanceDifference(distanceMap.get(a1), distanceMap.get(a2))).get()));
+    Asteroid getVaporizedAsteroid(int number, boolean print) {
+        boolean allDestroyed = false;
+        int counter = 0;
+        Map<Double, Queue<Asteroid>> angleMapCopy = new TreeMap<>(angleMap);
+        while (!allDestroyed) {
+            allDestroyed = true;
+            for (Map.Entry<Double, Queue<Asteroid>> entry : angleMapCopy.entrySet()) {
+                if (!entry.getValue().isEmpty()) {
+                    counter++;
+                    if (print) System.out.println(counter + " - " + entry.getValue().peek());
+                    if (counter == number) return entry.getValue().peek();
+                    entry.getValue().poll();
+                    allDestroyed = false;
+                }
+            }
+        }
+        return null;
     }
 
-    public List<Asteroid> getVisibleList() {
-        return visibleList;
-    }
-
-    public Map<Double, Set<Asteroid>> getAngleMap() {
+    public Map<Double, Queue<Asteroid>> getAngleMap() {
         return angleMap;
     }
 
-    public Map<Asteroid, Coords> getDistanceMap() {
-        return distanceMap;
+    public int getNumberRepresentation() {
+        return coords.x * 100 + coords.y;
     }
 
     @Override
@@ -97,11 +105,6 @@ class Coords {
     }
 
     static int distanceDifference(Coords c1, Coords c2) {
-        return Math.abs(c1.x) + Math.abs(c1.y) - Math.abs(c2.x) - Math.abs(c2.y);
-    }
-
-    @Override
-    public String toString() {
-        return "[" + x + "," + y + "]";
+        return Math.abs(c1.x) + Math.abs(c1.y) - (Math.abs(c2.x) + Math.abs(c2.y));
     }
 }
