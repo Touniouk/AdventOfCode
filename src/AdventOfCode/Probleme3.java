1cd03a36a6df6fb7c018c6dbafafe104f895e9bc
Touniouk <luc.bhayward@gmail.com>
2019-12-16 12:43:11 +0100

    added todos and removed redundant code

--- src/AdventOfCode/Probleme3.java
+++ src/AdventOfCode/Probleme3.java
@@ -15,21 +15,21 @@ public class Probleme3 {
         List<Wire> wires = Files.lines(Paths.get("input3.txt"))
                 .map(line -> new Wire(line.split(",")))
                 .collect(Collectors.toList());
-        HashSet<Point> set = new HashSet<>(wires.get(0).points.keySet());
+        HashSet<Coords> set = new HashSet<>(wires.get(0).points.keySet());
         set.retainAll(new HashSet<>(wires.get(1).points.keySet()));
-        Point closest = set.stream().min(
-                (p1, p2) -> Math.abs(p1.xcoord) + Math.abs(p1.ycoord) - Math.abs(p2.xcoord) - Math.abs(p2.ycoord)
+        Coords closest = set.stream().min(
+                (p1, p2) -> Math.abs(p1.x) + Math.abs(p1.y) - Math.abs(p2.x) - Math.abs(p2.y)
         ).get();
-        Point lowestDelay = set.stream().min(
+        Coords lowestDelay = set.stream().min(
                 (p1, p2) -> wires.get(0).points.get(p1) + wires.get(1).points.get(p1) -
                         wires.get(0).points.get(p2) - wires.get(1).points.get(p2)
         ).get();
-        System.out.println("Closest point: " + closest + " : " + (Math.abs(closest.xcoord) + Math.abs(closest.ycoord)));
+        System.out.println("Closest point: " + closest + " : " + (Math.abs(closest.x) + Math.abs(closest.y)));
         System.out.println("Lowest delay point: " + lowestDelay + " : " + (wires.get(0).points.get(lowestDelay) + wires.get(1).points.get(lowestDelay)));
     }
 
     class Wire {
-        HashMap<Point, Integer> points;
+        HashMap<Coords, Integer> points;
 
         Wire(String[] bendStr) {
             points = new HashMap<>();
@@ -55,40 +55,11 @@ public class Probleme3 {
                         incrementy = 0;
                 }
                 while (distCounter > 0) {
-                    points.putIfAbsent(new Point((xcoord += incrementx), (ycoord += incrementy)), totalDistCounter);
+                    points.putIfAbsent(new Coords((xcoord += incrementx), (ycoord += incrementy)), totalDistCounter);
                     distCounter--;
                     totalDistCounter++;
                 }
             }
-        }
-    }
-
-    class Point {
-        int xcoord;
-        int ycoord;
-
-        Point(int xcoord, int ycoord) {
-            this.xcoord = xcoord;
-            this.ycoord = ycoord;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Point point = (Point) o;
-            return xcoord == point.xcoord &&
-                    ycoord == point.ycoord;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(xcoord, ycoord);
-        }
-
-        @Override
-        public String toString() {
-            return "[" + xcoord + "," + ycoord + "]";
         }
     }
 
