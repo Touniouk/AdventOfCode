59912bf57cbbabce6390534d551d1d47f9c498d7
Luc <touniouk@yahoo.com>
2019-12-22 23:58:48 +0100

    Finished prob 15

--- src/AdventOfCode/Probleme15.java
+++ src/AdventOfCode/Probleme15.java
@@ -1,6 +1,7 @@
 package AdventOfCode;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.List;
 import java.util.stream.IntStream;
 
 import static java.lang.Math.toIntExact;
@@ -24,15 +26,19 @@ public class Probleme15 {
     private static final char EMPTY = ' ';
     private static final char UNKNOWN = '?';
     private static final char OX_SYST = '$';
-    private static final char H_BORDER = '=';
+    private static final char OXYGEN = 'O';
     private static final char V_BORDER = '|';
 
     private static long[] memory;
     private Logger logger;
+    private GameGUI GUI;
     private Map<Coords, Character> gameBoardTiles = new HashMap<>();
     private final Coords startingPosition = new Coords(GAMEBOARD_WIDTH/2, GAMEBOARD_HEIGHT/2);
     private Coords droidPosition = startingPosition;
+    private Coords oxygenPosition;
     private char droidChar = DROID;
+    private Stack<Integer> inputs = new Stack<>();
+    private boolean allMapExplored = false;
 
     public static void main(String... args) throws IOException {
         memory = Arrays.stream(Files.lines(Paths.get("input15.txt"))
@@ -45,46 +51,15 @@ public class Probleme15 {
     private void solveWithGUI() {
         // Put robot in the middle of the board
         gameBoardTiles.put(droidPosition, DROID);
-        IntcodeComputer computer = new IntcodeComputer(memory, LogLevel.IO);
-        new GameGUI(computer, false).buildAndShowGUI();
+        IntcodeComputer computer = new IntcodeComputer(memory, LogLevel.QUIET);
+        GUI = new GameGUI(computer);
+        GUI.buildAndShowGUI();
         new Thread(computer).start();
-    }
 
-    private String getGameBoard() {
-        StringBuilder builder = new StringBuilder();
-        Character c;
-
-        logger.debug("droidPosition: " + droidPosition + ", facing " + gameBoardTiles.get(droidPosition));
-
-        int lowest;
-        if (droidPosition.y > GAMEBOARD_HEIGHT-3) lowest = droidPosition.y - (GAMEBOARD_HEIGHT-3);
-        else if (droidPosition.y < 1) lowest = droidPosition.y-2;
-        else lowest = 0;
-
-        int leftest;
-        if (droidPosition.x > GAMEBOARD_WIDTH-3) leftest = droidPosition.x - (GAMEBOARD_WIDTH-3);
-        else if (droidPosition.x < 1) leftest = droidPosition.x - 2;
-        else leftest = 0;
-
-        logger.debug("lowest: " + lowest + " , Leftest: " + leftest);
-        for (int y = GAMEBOARD_HEIGHT-2+lowest; y >= lowest; y--) {
-            // Print left side border
-            builder.append(V_BORDER);
-            for (int x = leftest; x < GAMEBOARD_WIDTH-2-leftest; x++) {
-                if ((c = gameBoardTiles.get(new Coords(x, y))) != null) builder.append(c);
-                else builder.append(UNKNOWN);
-            }
-            // Print right side border
-            builder.append(V_BORDER + "\n");
+        while (!allMapExplored) {
+            GUI.UIStep();
         }
-        // Print upper and lower border
-        for (int x = 0; x < GAMEBOARD_WIDTH; x++) {
-            builder.append(H_BORDER);
-            builder.insert(0, H_BORDER);
-        }
-        builder.insert(GAMEBOARD_WIDTH, "\n");
-        logger.log(LogLevel.RIDICULOUS, builder.toString());
-        return builder.toString();
+        fillWithOxygen();
     }
 
     private String printFullGameBoard() {
@@ -95,10 +70,10 @@ public class Probleme15 {
         int leftest  = gameBoardTiles.keySet().stream().min(Comparator.comparingInt(k -> k.x)).get().x;
         int rightest = gameBoardTiles.keySet().stream().max(Comparator.comparingInt(k -> k.x)).get().x;
 
-        for (int y = highest+2; y >= lowest-2; y--) {
+        for (int y = highest; y >= lowest; y--) {
             // Print left side border
             builder.append(V_BORDER);
-            for (int x = leftest-2; x < rightest+2; x++) {
+            for (int x = leftest; x < rightest; x++) {
                 if ((c = gameBoardTiles.get(new Coords(x, y))) != null) builder.append(c);
                 else builder.append(UNKNOWN);
             }
@@ -123,13 +98,48 @@ public class Probleme15 {
             case 2:
                 // The repair droid has moved one step in the requested direction;
                 // its new position is the location of the oxygen system.
+                oxygenPosition = getTileInFrontOfDroid(input);
                 gameBoardTiles.put(droidPosition, EMPTY);
                 gameBoardTiles.put(getTileInFrontOfDroid(input), OX_SYST);
                 droidPosition = getTileInFrontOfDroid(input);
                 break;
         }
         gameBoardTiles.put(startingPosition, DROID);
+        if (oxygenPosition != null) gameBoardTiles.put(oxygenPosition, OX_SYST);
         gameBoardTiles.put(droidPosition, droidChar);
+        if (droidPosition.equals(startingPosition) && gameBoardTiles.size() > 10) {
+            logger.info("Full Map explored");
+            allMapExplored = true;
+        }
+    }
+
+    private void fillWithOxygen() {
+        logger.debug("Filling with Oxygen");
+        Queue<Coords> edges = new ArrayDeque<>();
+        Queue<Coords> oldEdges = new ArrayDeque<>();
+        edges.add(oxygenPosition);
+        int time = -1;
+        while (!edges.isEmpty()) {
+            while (!edges.isEmpty()) oldEdges.add(edges.poll());
+            while (!oldEdges.isEmpty()) {
+                Coords edge = oldEdges.poll();
+                Coords[] surroundings = new Coords[] {
+                        new Coords(edge.x + 1, edge.y),
+                        new Coords(edge.x - 1, edge.y),
+                        new Coords(edge.x, edge.y - 1),
+                        new Coords(edge.x, edge.y + 1)
+                };
+                for (Coords surrounding : surroundings) {
+                    if (gameBoardTiles.get(surrounding) != null && gameBoardTiles.get(surrounding) == EMPTY) {
+                        edges.add(surrounding);
+                        gameBoardTiles.put(surrounding, OXYGEN);
+                    }
+                }
+            }
+            time++;
+            GUI.timeLabel.setText("Time: " + time + " minutes");
+            GUI.updateGUI(printFullGameBoard());
+        }
     }
 
     private int getOppositeDirection(int direction) {
@@ -148,16 +158,6 @@ public class Probleme15 {
             case 2: return 4;
             case 3: return 2;
             case 4: return 1;
-        }
-        return 0;
-    }
-
-    private int getRight(int direction) {
-        switch (direction) {
-            case 1: return 4;
-            case 2: return 3;
-            case 3: return 1;
-            case 4: return 2;
         }
         return 0;
     }
@@ -186,49 +186,28 @@ public class Probleme15 {
         private IntcodeComputer computer;
         private JFrame frame;
         private JTextArea mainArea;
-        private boolean playWithUI;
+        private JLabel stepsLabel;
+        private JLabel timeLabel;
         private int facing = 1;
 
         GameGUI(IntcodeComputer computer) {
             this.computer = computer;
-            playWithUI = false;
         }
 
-        GameGUI(IntcodeComputer computer, boolean playWithUI) {
-            this(computer);
-            this.playWithUI = playWithUI;
-        }
-
-        private void step(int direction) {
-            processStatus(toIntExact(sendInput(direction)), direction);
-        }
-
-        private long cheatStep(int direction) {
-            // Do given direction
+        private long step(int direction) {
             long status = sendInput(direction);
             processStatus(toIntExact(status), direction);
-            // Look into other direction
-            logger.debug("Looking into other directions");
-            IntStream.of(1, 2, 3, 4).forEach(i -> {
-                long innerStatus = sendInput(i);
-                processStatus(toIntExact(innerStatus), i);
-                if (innerStatus == 1 || innerStatus == 2) {
-                    // We moved, backtrack
-                    int oppositeDirection = getOppositeDirection(i);
-                    processStatus(toIntExact(sendInput(oppositeDirection)), oppositeDirection);
-                }
-            });
-            logger.debug("CheatStep done");
+            if (status == 1  || status == 2) updateGUI(printFullGameBoard());
             return status;
         }
 
         private void UIStep() {
             logger.debug("Facing " + getDirectionName(facing) +
                     ", trying to go " + getDirectionName(getLeft(facing)));
-            if (cheatStep(getLeft(facing)) == 0) {
+            if (step(getLeft(facing)) == 0) {
                 logger.debug("Couldn't go " + getDirectionName(getLeft(facing)) +
                         ", trying to go straight (" + getDirectionName(facing) + ")");
-                if (cheatStep(facing) == 0) setFacing(getRight(facing));
+                if (step(facing) == 0) setFacing(getOppositeDirection(getLeft(facing)));
             } else setFacing(getLeft(facing));
             logger.debug("UIStep done, facing " + getDirectionName(facing));
         }
@@ -248,9 +227,14 @@ public class Probleme15 {
         private long sendInput(int input) {
             long output = computer.POISON;
             try {
-                if (computer.getInputQueue().take() == computer.POISON) close();
+                computer.getInputQueue().take();
                 computer.getInputQueue().put((long) input);
-                if ((output = computer.getOutputQueue().take()) == computer.POISON) close();
+                output = computer.getOutputQueue().take();
+                if (output > 0 && oxygenPosition != null) {
+                    if (inputs.isEmpty() || inputs.peek() != getOppositeDirection(input)) inputs.push(input);
+                    else inputs.pop();
+                    stepsLabel.setText("Steps: " + inputs.size());
+                }
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
@@ -262,68 +246,27 @@ public class Probleme15 {
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLocationRelativeTo(null);
             JPanel mainComp = new JPanel(new BorderLayout());
-            mainArea = new JTextArea(GAMEBOARD_HEIGHT+1, GAMEBOARD_WIDTH+1);
+            mainArea = new JTextArea(1, 1);
             mainArea.setEditable(false);
             mainArea.setBorder(BorderFactory.createEmptyBorder(5, 10, 15, 10));
             mainArea.setFont(new Font("Courier New", Font.PLAIN, 14));
-            mainArea.setText(getGameBoard());
-            mainArea.addKeyListener(new KeyListener() {
-                @Override
-                public void keyTyped(KeyEvent e) {}
-                @Override
-                public void keyPressed(KeyEvent e) {
-                    if (playWithUI) {
-                        UIStep();
-                        updateGUI(getGameBoard());
-                        return;
-                    }
-                    switch(e.getKeyCode()) {
-                        case KeyEvent.VK_LEFT:
-                            droidChar = DROID_LEFT;
-                            cheatStep(3);
-                            break;
-                        case KeyEvent.VK_RIGHT :
-                            droidChar = DROID_RIGHT;
-                            cheatStep(4);
-                            break;
-                        case KeyEvent.VK_UP:
-                            droidChar = DROID_UP;
-                            cheatStep(1);
-                            break;
-                        case KeyEvent.VK_DOWN:
-                            droidChar = DROID_DOWN;
-                            cheatStep(2);
-                            break;
-                    }
-                    updateGUI(getGameBoard());
-                }
-                @Override
-                public void keyReleased(KeyEvent e) { }
-            });
-            JButton printButton = new JButton("Print Map");
-            printButton.addActionListener(e -> {
-                System.out.println("\n\n\n");
-                System.out.println(printFullGameBoard());
-                System.out.println("\n");
-            });
+            mainArea.setText(printFullGameBoard());
             mainComp.add(mainArea, BorderLayout.CENTER);
-            mainComp.add(printButton, BorderLayout.SOUTH);
+            JPanel northPanel = new JPanel(new GridLayout(1, 2));
+            timeLabel = new JLabel("Time: 0 minutes");
+            stepsLabel = new JLabel("Steps: ");
+            northPanel.add(stepsLabel);
+            northPanel.add(timeLabel);
+            mainComp.add(northPanel, BorderLayout.NORTH);
             frame.getContentPane().add(mainComp);
             frame.pack();
-
-            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-            frame.setLocation((dim.width-frame.getWidth())/2, (dim.height-frame.getHeight())/2);
-
             frame.setVisible(true);
         }
 
         void updateGUI(String text) {
             mainArea.setText(text);
             frame.validate();
-        }
-
-        void close() {
-            frame.dispose();
+            frame.setSize(frame.getPreferredSize());
         }
     }
 }
